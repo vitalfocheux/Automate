3f993d62d6f27e7a51a0c0229e6c237009c6719b
vitalfocheux <vitalfocheux@gmail.com>
2024-01-03 15:06:14 +0100

    Modif des configurations

--- Ruby/spec/Automaton_spec_helper.rb
+++ Ruby/spec/Automaton_spec_helper.rb
@@ -3,6 +3,7 @@ require 'rspec'
 RSpec.configure do |config|
     config.formatter = :documentation # Pour afficher des détails pour tous les tests
     config.color_mode = :on # Activer la coloration pour une meilleure lisibilité
-    # Autres configurations...
+    config.warnings = true # Activer les warnings
+    config.raise_errors_for_deprecations! # Afficher les erreurs pour les méthodes dépréciées
 end
   
